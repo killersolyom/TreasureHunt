d96cd2adcca6fd9758046b7df873e8deb6e89239
iNTeRSHiP <smartpath10@3ss.tv>
2019-07-15 15:51:59 +0300

    Implemented MainActivity & Fragments create Created by: Solyom, Mille

---
+++ app/src/main/java/com/threess/summership/treasurehunt/ClaimTreasureFragment.java
@@ -0,0 +1,34 @@
+package com.threess.summership.treasurehunt;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ClaimTreasureFragment extends Fragment {
+
+    public static String TAG = "claim_treasure_fragment";
+
+    public ClaimTreasureFragment() {
+        // constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_claim_treasure, container, false);
+        // Do not modify!
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO
+
+    }
+}
---
+++ app/src/main/java/com/threess/summership/treasurehunt/LoginRegistrationFragment.java
@@ -0,0 +1,34 @@
+package com.threess.summership.treasurehunt;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LoginRegistrationFragment extends Fragment {
+
+    public static String TAG = "login_registration_fragment";
+
+    public LoginRegistrationFragment() {
+        // constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login_registration, container, false);
+        // Do not modify!
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO
+
+    }
+}
