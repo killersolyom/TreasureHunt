55d9e110aa122461c6fa4cd7c0cfa505ba4ed73a
Mille Janos <smartpath12@3ss.tv>
2019-07-15 14:36:01 +0300

    develop branch created

--- app/src/main/java/com/threess/summership/treasurehunt/MainActivity.java
+++ app/src/main/java/com/threess/summership/treasurehunt/MainActivity.java
@@ -13,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // develop
+
     }
 }
