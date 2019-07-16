0339715eb01935bfd237e4289a5e1e319fe56a23
iNTeRSHiP <smartpath3@3ss.tv>
2019-07-16 14:24:22 +0300

    implemented shared preferences class

--- app/src/main/java/com/threess/summership/treasurehunt/model/User.java
+++ app/src/main/java/com/threess/summership/treasurehunt/model/User.java
@@ -9,11 +9,6 @@ public class User {
     @SerializedName("password")
     private String password;
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
     public String getUsername() {
         return username;
     }
