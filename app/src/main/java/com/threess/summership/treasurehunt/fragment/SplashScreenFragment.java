4b9720ada7765b12c191706131b1a94ef4e3c10a
iNTeRSHiP <smartpath10@3ss.tv>
2019-07-17 10:50:17 +0300

    MapView focus change

--- app/src/main/java/com/threess/summership/treasurehunt/fragment/SplashScreenFragment.java
+++ app/src/main/java/com/threess/summership/treasurehunt/fragment/SplashScreenFragment.java
@@ -23,14 +23,10 @@ public class SplashScreenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_splash_screen, container, false);
-        // Do not modify!
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // TODO
-
     }
 }
