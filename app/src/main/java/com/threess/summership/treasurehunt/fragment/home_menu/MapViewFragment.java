4f053880f231505a6488d02447c7fbbe0cb65089
iNTeRSHiP <smartpath10@3ss.tv>
2019-07-18 10:59:11 +0300

    testing

--- app/src/main/java/com/threess/summership/treasurehunt/fragment/home_menu/MapViewFragment.java
+++ app/src/main/java/com/threess/summership/treasurehunt/fragment/home_menu/MapViewFragment.java
@@ -1,5 +1,8 @@
 package com.threess.summership.treasurehunt.fragment.home_menu;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,15 +15,26 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.threess.summership.treasurehunt.R;
+import com.threess.summership.treasurehunt.logic.ApiController;
+import com.threess.summership.treasurehunt.model.Treasure;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MapViewFragment extends Fragment implements OnMapReadyCallback {
 
     private MapView mMapView;
     public static String TAG = "MapView_fragment";
+    private ArrayList<Treasure> treasures = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,13 +43,17 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
         mMapView.getMapAsync(this);
-
+        getTreasures();
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
             e.printStackTrace();
         }
         return rootView;
+    }
+
+    private void drawMap(){
+        mMapView.getMapAsync(this);
     }
 
     @Override
@@ -65,6 +83,31 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        changeFocus(googleMap);
+        drawMarkers(googleMap);
+    }
+
+    private void drawMarkers(GoogleMap googleMap){
+        for(Treasure it: treasures){
+            googleMap.addMarker(
+                    new MarkerOptions().position(
+                            new LatLng(it.getLocation_lat(),
+                                    it.getLocation_lon())).title(it.getDescription()))
+                    .setIcon(BitmapDescriptorFactory.fromBitmap(getDrawableImage()));
+        }
+    }
+
+    private Bitmap getDrawableImage(){
+        return Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.app_icon),100,100,false);
+    }
+
+    private BitmapDescriptor getMarkerIcon(String color) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(Color.parseColor(color), hsv);
+        return BitmapDescriptorFactory.defaultMarker(hsv[0]);
+    }
+
+    private void changeFocus(GoogleMap googleMap){
         MarkerOptions marker = new MarkerOptions().position(new LatLng(46.544595, 24.561126));
         googleMap.moveCamera( changeFocus(marker));
         googleMap.animateCamera( CameraUpdateFactory.zoomTo( 15.0f ) );
@@ -74,6 +117,22 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback {
         LatLngBounds.Builder builder = new LatLngBounds.Builder().include(position.getPosition());
         LatLngBounds bounds = builder.build();
         return CameraUpdateFactory.newLatLngBounds(bounds, 0);
+    }
+
+    private void getTreasures(){
+        ApiController.getInstance().getAllTreasures(new Callback<ArrayList<Treasure>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Treasure>> call, Response<ArrayList<Treasure>> response) {
+                treasures.clear();
+                treasures.addAll(response.body());
+                drawMap();
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Treasure>> call, Throwable t) {
+
+            }
+        });
     }
 
 }
