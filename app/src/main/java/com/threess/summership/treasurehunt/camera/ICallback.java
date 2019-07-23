1c99623dc515ac89a10642f7e66ab9f42c9bc94e
smiley2016 <smileyb666@gmail.com>
2019-07-23 14:27:55 +0300

    Camera

---
+++ app/src/main/java/com/threess/summership/treasurehunt/camera/ICallback.java
@@ -0,0 +1,12 @@
+package com.threess.summership.treasurehunt.camera;
+
+import java.io.File;
+
+/**
+ * Created by User on 6/4/2018.
+ */
+
+public interface ICallback {
+
+    void done(Exception e, File file);
+}
---
+++ app/src/main/java/com/threess/summership/treasurehunt/fragment/home_menu/FavoriteTreasureFragment.java
@@ -0,0 +1,63 @@
+package com.threess.summership.treasurehunt.fragment.home_menu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.threess.summership.treasurehunt.R;
+import com.threess.summership.treasurehunt.logic.ApiController;
+import com.threess.summership.treasurehunt.model.Treasure;
+import com.threess.summership.treasurehunt.adapter.TreasureAdapter;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class FavoriteTreasureFragment extends Fragment {
+
+    private RecyclerView recycle;
+    private TreasureAdapter adapter;
+    private ArrayList<Treasure> treasures = new ArrayList<>();
+
+    public FavoriteTreasureFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_treasure, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recycle = view.findViewById(R.id.recycler_view_contacts);
+        adapter = new TreasureAdapter(this.getContext());
+        recycle.setAdapter(adapter);
+        recycle.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        getTreasures();
+    }
+
+    private void getTreasures(){
+        ApiController.getInstance().getAllTreasures(new Callback<ArrayList<Treasure>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Treasure>> call, Response<ArrayList<Treasure>> response) {
+                adapter.addTreasure(response.body());
+            }
+            @Override
+            public void onFailure(Call<ArrayList<Treasure>> call, Throwable t) {
+
+            }
+        });
+    }
+}
+
